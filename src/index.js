e311d3aefd050f04ee4b98729c2bff3d55c3443c
Boniface <Cheruiyotbony@gmail.com>
2023-10-22 15:09:53 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  document.querySelector('form').addEventListener("submit",(e)=>{
+    e.preventDefault()
+    buildtask(e.target.new_task_description.value)
+  })
+
+function buildtask(tassk){
+  let p=document.createElement('p')
+  let btn=document.createElement('button')
+  btn.textContent='x'
+  btn.addEventListener('click',handleDelete)
+  p.textContent= `${tassk} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
 });
